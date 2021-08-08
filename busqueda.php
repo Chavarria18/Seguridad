33ad37dc3ffba7f7b89e839bc512c2bfc9154324
EvelynDubon99 <dubon181014@unis.edu.gt>
2021-08-07 19:52:00 -0600

    css

--- busqueda.php
+++ busqueda.php
@@ -6,14 +6,14 @@
     //require_once('/opt/lampp/htdocs/Seguridad/conexion.php') ; 
   //$conexion = mysqli_connect('127.0.0.1', 'root', '', 'Seguridad');
   $mysqli = new mysqli("127.0.0.1", "root", "", "Seguridad");
-  print_r($_GET);
+  //print_r($_GET);
   $busqueda = $_GET['input_busqueda'];
 
   if (mysqli_connect_errno()) {
     printf("Conexión fallida: %s\n", mysqli_connect_error());
     exit();
 }else{
-    printf("WRYYYYYYY"); 
+    //printf("WRYYYYYYY"); 
 }
 ?> 
 
@@ -24,19 +24,32 @@
         <title>Home</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="http://localhost/Seguridad/homes.css">
     </head>
+    <header>
+    <nav class="navbar navbar-expand-xl navbar_intems " id="l"> <a class="navbar-brand" href="#">Home</a> 
+        <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"> 
+          <span class="navbar-toggler-icon" id="narv"></span> </button> <div class="collapse navbar-collapse" id="navbarNavAltMarkup"> 
+            <div class="navbar-nav"> <a class="nav-item nav-link active" href="#">Link<span class="sr-only">(current)</span></a> 
+                <a class="nav-item nav-link disabled" href="#"></a> </div> </div>
+              </nav>
+
+    </header>
     <body>
-<h1>Se busco:</h1>
-        <div>
+        <br>
+        <h1 class="text-center">Se busco:</h1>
+        <div class="text-center">
         
+        <h3 class="text-center">
             <?php
                     
+                  
                     try {
                        // $pdo = new PDO($dsn, $user, $pass, $opt);
                         //$sql = "select *from PRODUCTO where nombre_p ='" .$busqueda. "'";
                         $sql = "select *from PRODUCTO where NOMBRE_P = '".$_GET['input_busqueda']."' " ; 
-                        echo print_r($sql);
+                       echo print_r($_GET['input_busqueda']);
                        // $sql2 = $pdo -> query($sql);
                         
                         $mysqli -> multi_query($sql);
@@ -46,13 +59,29 @@
                         while ($enumerar = $result->fetch_row()) {
 
                         ?>
-                       
-                        <h2><?php echo $enumerar[1] ?></h2> 
-                        <p>
-                        <?php echo $enumerar[2] ?> 
-                        <?php echo $enumerar[3] ?> 
-                        
-                        </p>
+        </h3>    
+        <div class="col-md-6">
+            <h3 class="text-center"> Producto </h3>
+            <div class=" table-responsive-md">
+                <table class="table table-bordered table-hover">
+                <thead>
+                    <tr>
+                    <th>Nombre</th>
+                    <th>Foto</th>
+                    <th>Descripcion</th>
+                   
+                    </tr>
+                </thead>
+                <tbody>
+                <tr> 
+                          
+                          <td><?php echo $enumerar[1] ?></td>
+                      
+                         <td><?php echo $enumerar[2] ?> </td>
+                         <td><?php echo $enumerar[3] ?> </td>
+                      
+                      
+                      
                         
                       
 
@@ -64,6 +93,9 @@
 
 
                     ?>
+                    </tr>
+                </tbody>
+                </table>
         </div>
 
 
--- home.php
+++ home.php
@@ -13,18 +13,52 @@
         <title>Home</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="http://localhost/Seguridad/homes.css">
     </head>
+    <header>
+    <nav class="navbar navbar-expand-xl navbar_intems " id="l"> <a class="navbar-brand" href="#">Home</a> 
+        <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"> 
+          <span class="navbar-toggler-icon" id="narv"></span> </button> <div class="collapse navbar-collapse" id="navbarNavAltMarkup"> 
+            <div class="navbar-nav"> <a class="nav-item nav-link active" href="#">Link<span class="sr-only">(current)</span></a> 
+                <a class="nav-item nav-link disabled" href="#"></a> </div> </div>
+              </nav>
+
+    </header>
     <body>
-<div>
-
-<form method="GET" action ="https://localhost/Seguridad/busqueda.php">
-    <input name="input_busqueda">
-    <button type="submit">Buscar</button>
-</form>
-</div>
-
-    <div>
+        <div class= "container">
+        <br>
+        <br>
+        <div class="row">
+            <div class="col-md-3">
+            </div>
+          <div class="col-md-6">
+        <form id="form" method="GET" action ="https://localhost/Seguridad/busqueda.php">
+            <input name="input_busqueda" class="form-control">
+            <br>
+            <button type="submit" class="btn btn-lg btn-primary btn-block" id="primary" >Buscar</button>
+        </form>
+        </div>
+        <div class="col-md-3">
+            </div>
+        </div>
+        </div>
+<br>
+    <div class="col-md-3">
+    </div>    
+        <div class="col-md-6">
+            <h3 class="text-center"> Producto </h3>
+            <div class=" table-responsive-md">
+                <table class="table table-bordered table-hover">
+                <thead>
+                    <tr>
+                    <th>Nombre</th>
+                    <th>Foto</th>
+                    <th>Descripcion</th>
+                   
+                    </tr>
+                </thead>
+                <tbody>
         
             <?php
                     
@@ -35,12 +69,14 @@
                         while ($enumerar = mysqli_fetch_array($result)) {
                         ?>
                         
-                        <h2><?php echo $enumerar[1] ?></h2> 
-                        <p>
-                        <?php echo $enumerar[2] ?> 
-                        <?php echo $enumerar[3] ?> 
+                       <tr> 
+                          
+                            <td><?php echo $enumerar[1] ?></td>
                         
-                        </p>
+                           <td><?php echo $enumerar[2] ?> </td>
+                           <td><?php echo $enumerar[3] ?> </td>
+                        
+                        
                         
                       
 
@@ -52,7 +88,12 @@
 
 
                     ?>
+                       </tr>
+                </tbody>
+                </table>
     </div>
+    <div class="col-md-3">
+    </div>  
 
 
 
@@ -62,4 +103,15 @@
      
         <script src="" async defer></script>
     </body>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+    crossorigin="anonymous"></script>
 </html>
